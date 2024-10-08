830f9879ec5ad5772fdf0fc004e57376682ef2e7
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2024-10-08 13:43:20 +0200

    wip

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> Renewex Operational Job Quiz </h1>
+
+<p align="center">
+  <a href="https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=868068523">
+      <img src="https://github.com/codespaces/badge.svg" alt="Fork and Open in GitHub Codespaces" onerror="this.style.display='none'">
+  </a>
+</p>
---
+++ models.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from datetime import date, datetime
+from typing import Annotated, Any
+
+import sqlalchemy as sa
+from pandas.core.interchange.dataframe_protocol import enum
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    type_annotation_map: dict[type[Any], sa.types.TypeEngine[Any]] = {
+        datetime: sa.types.TIMESTAMP(timezone=True),
+        float: Annotated[sa.types.Numeric, 10, 2],
+    }
+
+
+class Techno(enum.StrEnum):
+    wind_turbine_onshore = enum.auto()
+    wind_turbine_offshore = enum.auto()
+
+    solar_field_ground_mounted = enum.auto()
+    solar_field_rooftop = enum.auto()
+    solar_field_canopy = enum.auto()
+
+    hydro_turbine_run_of_river = enum.auto()
+    hydro_turbine_pumped_storage = enum.auto()
+    hydro_turbine_reservoir = enum.auto()
+
+    cogeneration_biomass = enum.auto()
+    cogeneration_waste = enum.auto()
+    cogeneration_other = enum.auto()
+
+
+class Site(Base):
+    __tablename__ = "sites"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String(100), unique=True)
+    capacity: Mapped[float] = mapped_column(doc="Installed capacity in [kW]")
+    techno: Mapped[Techno] = mapped_column()
+
+    contracts: Mapped[list["Contract"]] = relationship(back_populates="site")
+
+
+class Contract(Base):
+    __tablename__ = "contracts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    purchase_order: Mapped[str] = mapped_column(sa.String(50), unique=True)
+
+    start_date: Mapped[date] = mapped_column(sa.Date)
+    end_date: Mapped[date] = mapped_column(sa.Date)
+
+    site_id: Mapped[int] = mapped_column(sa.ForeignKey("sites.id"))
+    site: Mapped["Site"] = relationship(back_populates="contracts")
+
+    invoices: Mapped[list["Invoice"]] = relationship(back_populates="contract")
+
+
+class InvoiceStatus(enum.StrEnum):
+    draft = enum.auto()
+    computed = enum.auto()
+    error = enum.auto()
+    published = enum.auto()
+    paid = enum.auto()
+
+
+class Invoice(Base):
+    __tablename__ = "invoices"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    publication_id: Mapped[str] = mapped_column(sa.String(50), unique=True)
+    issued_at: Mapped[datetime] = mapped_column(server_default=sa.func.now())
+    amount: Mapped[float] = mapped_column()
+
+    status: Mapped[InvoiceStatus] = mapped_column()
+
+    contract_id: Mapped[int] = mapped_column(sa.ForeignKey("contracts.id"))
+    contract: Mapped["Contract"] = relationship(back_populates="invoices")
+
+
+mapper_registry = sa.orm.registry()
+mapper_registry.update_type_annotation_map(
+    {Techno: sa.String, InvoiceStatus: sa.String}
+)
