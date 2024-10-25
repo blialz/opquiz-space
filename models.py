87ff0f8ae49fca9f07c6cd0329671583ce1ad207
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2024-10-25 09:28:39 +0200

    upt(space)

--- models.py
+++ models.py
@@ -2,10 +2,16 @@ from __future__ import annotations
 
 import enum
 from datetime import date, datetime
-from typing import Any
+from typing import Any, Literal
+from zoneinfo import ZoneInfo
 
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+)
 
 
 class Base(DeclarativeBase):
@@ -42,7 +48,12 @@ class Site(Base):
     capacity: Mapped[float] = mapped_column(doc="Installed capacity in [kW]")
     techno: Mapped[Techno] = mapped_column()
 
-    contracts: Mapped[list["Contract"]] = relationship(back_populates="site")
+    latitude: Mapped[float] = mapped_column()
+    longitude: Mapped[float] = mapped_column()
+
+    contracts: Mapped[list[Contract]] = relationship(back_populates="site")
+
+    ts_records: Mapped[list[TSRecords]] = relationship(back_populates="site")
 
     def __str__(self) -> str:
         return self.name
@@ -60,7 +71,12 @@ class Contract(Base):
     site_id: Mapped[int] = mapped_column(sa.ForeignKey("sites.id"))
     site: Mapped["Site"] = relationship(back_populates="contracts")
 
-    invoices: Mapped[list["Invoice"]] = relationship(back_populates="contract")
+    price: Mapped[float] = mapped_column()
+    invoicing_frequency: Mapped[
+        Literal["Monthly", "Quarterly", "Bi-annually", "Annually"]
+    ] = mapped_column()
+
+    invoices: Mapped[list[Invoice]] = relationship(back_populates="contract")
 
     def __str__(self) -> str:
         return self.purchase_order
@@ -79,17 +95,39 @@ class Invoice(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
+    start_date: Mapped[date] = mapped_column(sa.Date)
+    end_date: Mapped[date] = mapped_column(sa.Date)
+
     publication_id: Mapped[str] = mapped_column(sa.String(50), unique=True)
-    issued_at: Mapped[datetime] = mapped_column(server_default=sa.func.now())
     amount: Mapped[float] = mapped_column()
+    amount_unit: Mapped[str] = mapped_column()
 
     status: Mapped[InvoiceStatus] = mapped_column()
 
     contract_id: Mapped[int] = mapped_column(sa.ForeignKey("contracts.id"))
-    contract: Mapped["Contract"] = relationship(back_populates="invoices")
+    contract: Mapped[Contract] = relationship(back_populates="invoices")
 
     def __str__(self) -> str:
         return f"{self.publication_id} - {self.status}"
+
+
+class TSRecords(Base):
+    __tablename__ = "timeseries_records"
+
+    site_id: Mapped[int] = mapped_column(sa.ForeignKey("sites.id"), primary_key=True)
+    site: Mapped["Site"] = relationship(back_populates="ts_records")
+
+    timestamp: Mapped[datetime] = mapped_column(primary_key=True)
+
+    production: Mapped[float] = mapped_column()
+    unit_production: Mapped[str] = mapped_column()
+
+    cashflow: Mapped[float] = mapped_column()
+    unit_cashflow: Mapped[str] = mapped_column()
+
+    def __str__(self) -> str:
+        as_date = self.timestamp.astimezone(ZoneInfo("Europe/Paris")).date()
+        return f"{self.site} - {as_date.isoformat()}"
 
 
 mapper_registry = sa.orm.registry()
